f2bc4ae415b1f885b8858e0f73bc7785a6249ed8
Jacopo Margutti <jacopo.margutti@gmail.com>
2025-03-14 08:58:55 +0100

    bug fix translator

--- utils/translator.py
+++ utils/translator.py
@@ -1,11 +1,14 @@
 import requests
 import os
 from dotenv import load_dotenv
+import pandas as pd
 
 load_dotenv()
 
 
 def translate(from_lang: str, to_lang: str, text: str) -> str:
+    if pd.isna(text):
+        return ""
     translator_params = {"api-version": "3.0", "to": [to_lang], "from": [from_lang]}
     translator_headers = {
         "Ocp-Apim-Subscription-Key": os.getenv("MSCOGNITIVE_KEY"),
