84a87db47be4b156b74728442f5af47311ef31ca
Jacopo Margutti <jacopo.margutti@gmail.com>
2026-05-08 14:00:28 +0200

    return context and load offer sheet

--- utils/constants.py
+++ utils/constants.py
@@ -2,14 +2,26 @@ class DocumentMetadata:
     """Document class metadata keys"""
 
     __slots__ = ()
-    # vector store fields
+    # vector store fields for all sheets
     GOOGLE_INDEX = "google_index"
     CATEGORY = "categoryID"
     SUBCATEGORY = "subcategoryID"
     SLUG = "slug"
+    # vector store fields for Q&As sheet
     PARENT = "parent"
     QUESTION = "question"
     ANSWER = "answer"
+    # vector store fields for Offers sheet
+    NAME = "name"
+    DESCRIPTION = "desciption"
+    PHONENUMBERS = "phonenumbers"
+    EMAILS = "emails"
+    WEBURLS = "weburls"
+    ADDRESS = "address"
+    OPENWEEK = "openweek"
+    OPENWEEKEND = "openwekend"
+    NEEDTOKNOW = "needtoknow"
+    MOREINFO = "moreinfo"
     # splitting and embedding fields
     EMBEDDING_MODEL = "embedding_model"
     NTH_CHUNK = "nth_chunk"
